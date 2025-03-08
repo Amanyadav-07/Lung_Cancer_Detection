f4ac1b9d844001aa8f3abc23a40cc554ac913e68
Amanyadav-07 <amanyadav00fyit@gmail.com>
2025-03-08 22:19:22 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ from flask import Flask, request, render_template
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
+import os
+import gdown  # Install using: pip install gdown
+
 app = Flask(__name__)
+# Google Drive file ID
+file_id = "https://drive.google.com/file/d/1VPollmB85-K4jYdBtobQLZ0T02Jv8txG/view?usp=drive_link"
+
+# Destination file path
+model_path = "Lung_Cancer_model.keras"
+
+# Download the model if it doesn't exist
+if not os.path.exists(model_path):
+    print("Downloading model from Google Drive...")
+    gdown.download(f"https://drive.google.com/uc?id={file_id}", model_path, quiet=False)
+
+# Load the model
+model = load_model(model_path)
+print("Model loaded successfully!")
 
 # Load trained lung cancer model
 MODEL_PATH = "Lung_Cancer_model.keras"
